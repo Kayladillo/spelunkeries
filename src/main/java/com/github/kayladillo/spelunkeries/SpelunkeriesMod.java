ce9b7596e081edab392a83d63d3c9e40ffdc7025
Talrey <cycleman.atl@gmail.com>
2022-02-21 16:27:20 -0800

    first run success

---
+++ src/main/java/com/github/kayladillo/spelunkeries/SpelunkeriesMod.java
@@ -0,0 +1,62 @@
+package com.github.kayladillo.spelunkeries;
+
+import com.tterrag.registrate.Registrate;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("spelunkeries")
+public class SpelunkeriesMod
+{
+  public static final String MODID = "spelunkeries";
+  public static final Logger LOGGER = LogManager.getLogger();
+
+  private static Registrate registrar;
+  private static ItemRegistration items;
+  private static BlockRegistration blocks;
+//  private static BlockEntityRegistration tiles;
+
+  public SpelunkeriesMod () {
+    ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONF);
+    ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONF);
+
+//    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupCommon);
+//    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+//    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+//    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+    MinecraftForge.EVENT_BUS.register(this);
+
+    registrar = Registrate.create(MODID);
+    items = new ItemRegistration();
+    items.registerItems(registrar);
+    blocks = new BlockRegistration();
+    blocks.registerBlocks(registrar);
+//    tiles = new BlockEntityRegistration();
+//    tiles.registerBlockEntities(registrar);
+  }
+
+//  private void setupCommon (final FMLCommonSetupEvent event) {
+    // register capabilities here
+//  }
+
+//  private void setupClient (final FMLClientSetupEvent event) {
+//    LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+//  }
+
+//  private void enqueueIMC(final InterModEnqueueEvent event)
+//  {
+//    // some example code to dispatch IMC to another mod
+//    InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+//  }
+
+//  private void processIMC(final InterModProcessEvent event)
+//  {
+//    // some example code to receive and process InterModComms from other mods
+//    LOGGER.info("Got IMC {}", event.getIMCStream().
+//    map(m->m.getMessageSupplier().get()).
+//    collect(Collectors.toList()));
+//  }
+}
